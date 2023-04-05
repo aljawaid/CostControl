2c2263eb1eb285b3e445b83bb51b55a891539d25
root <creecros@gmail.com>
2023-04-05 22:01:27 +0000

    Fix Typo in templates change and create

--- Template/currency/change.php
+++ Template/currency/change.php
@@ -1,7 +1,8 @@
 <div class="page-header">
     <h2 class=""><?= t('Change reference currency') ?></h2>
 </div>
-<form method="post" class=""> action="<?= $this->url->href('CurrencyController', 'update') ?>" autocomplete="off">
+<form method="post" class="" action="<?= $this->url->href('CurrencyController', 'update') ?>" autocomplete="off">
+
     <?= $this->form->csrf() ?>
     <?= $this->form->label(t('Reference currency'), 'application_currency') ?>
     <?= $this->form->select('application_currency', $currencies, $values, $errors) ?>
--- Template/currency/create.php
+++ Template/currency/create.php
@@ -1,7 +1,7 @@
 <div class="page-header">
     <h2 class=""><?= t('Add or change currency rate') ?></h2>
 </div>
-<form method="post" class=""> action="<?= $this->url->href('CurrencyController', 'save') ?>" autocomplete="off">
+<form method="post" class="" action="<?= $this->url->href('CurrencyController', 'save') ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
     <?= $this->form->label(t('Currency'), 'currency') ?>
     <?= $this->form->select('currency', $currencies, $values, $errors) ?>

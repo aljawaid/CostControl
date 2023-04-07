134e983e2bf76c95bec2d94a262c65a80982e4c8
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-07 22:23:04 +0100

    Create CostControlLayoutHelper.php
    - Create `customLayout()`

---
+++ Helper/CostControlLayoutHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kanboard\Plugin\CostControl\Helper;
+
+use Kanboard\Core\Base;
+
+/**
+ * CostControlLayout Helper
+ *
+ * @package helper
+ * @author  Frederic Guillot
+ */
+class CostControlLayoutHelper extends Base
+{
+    /**
+     * Common layout for config views
+     *
+     * @access public
+     * @param  string $template
+     * @param  array  $params
+     * @return string
+     */
+    public function customLayout($template, array $params)
+    {
+        if (! isset($params['values'])) {
+            $params['values'] = $this->configModel->getAll();
+        }
+
+        if (! isset($params['errors'])) {
+            $params['errors'] = array();
+        }
+
+        return $this->helper->layout->subLayout('config/layout', 'costControl:cost-control/sidebar', $template, $params);
+    }
+}

dfbc9357dc19fc4f9bb7d51aded12acde60b511b
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-07 09:41:59 +0100

    Fix base currency wording in change.php

--- Template/currency/change.php
+++ Template/currency/change.php
@@ -1,10 +1,10 @@
 <div class="page-header">
-    <h2 class=""><?= t('Change reference currency') ?></h2>
+    <h2 class=""><?= t('Change Base Currency') ?></h2>
 </div>
-<form method="post" class="" action="<?= $this->url->href('CurrencyController', 'update') ?>" autocomplete="off">
+<form method="post" class="" action="<?= $this->url->href('CurrencyController', 'update') ?>" autocomplete="on">
 
     <?= $this->form->csrf() ?>
-    <?= $this->form->label(t('Reference currency'), 'application_currency') ?>
+    <?= $this->form->label(t('Base Currency'), 'application_currency') ?>
     <?= $this->form->select('application_currency', $currencies, $values, $errors) ?>
     <?= $this->modal->submitButtons() ?>
 </form>

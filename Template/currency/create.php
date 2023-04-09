705b80a3b46b8b011285e287bafb8d0c1c67d8e0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-09 20:48:03 +0100

    Fix template to honour comment settings

--- Template/currency/create.php
+++ Template/currency/create.php
@@ -1,15 +1,24 @@
 <div class="page-header">
-    <h2 class=""><?= t('Add or change currency rate') ?></h2>
+    <h2 class=""><?= t('Create or Update Manual Rate') ?></h2>
 </div>
-<form method="post" class="" action="<?= $this->url->href('CostController', 'save', array('plugin' => 'CostControl')) ?>" autocomplete="on">
-    <?= $this->form->csrf() ?>
-    <?= $this->form->label(t('Currency'), 'currency') ?>
-    <?= $this->form->select('currency', $currencies, $values, $errors) ?>
-    <?= $this->form->label(t('Rate'), 'rate') ?>
-    <?= $this->form->text('rate', $values, $errors, array('autofocus'), 'form-numeric') ?>
-    <?php if ($this->task->configModel->get('cost_control_rate_comments', '') == 'replace_comment_on_edit'): ?>
+<?php if ($this->task->configModel->get('cost_control_rate_comments', '') == 'replace_comment_on_edit'): ?>
+    <form method="post" class="" action="<?= $this->url->href('CostController', 'save', array('plugin' => 'CostControl')) ?>" autocomplete="on">
+        <?= $this->form->csrf() ?>
+        <?= $this->form->label(t('Currency'), 'currency') ?>
+        <?= $this->form->select('currency', $currencies, $values, $errors) ?>
+        <?= $this->form->label(t('Rate'), 'rate') ?>
+        <?= $this->form->text('rate', $values, $errors, array('autofocus'), 'form-numeric') ?>
         <?= $this->form->label(t('New Comment'), 'comment') ?>
         <?= $this->form->text('comment', $values, array(), array('placeholder="Provider Notes"'), 'form-text') ?>
-    <?php endif ?>
-    <?= $this->modal->submitButtons() ?>
-</form>
+        <?= $this->modal->submitButtons() ?>
+    </form>
+<?php elseif ($this->task->configModel->get('cost_control_rate_comments', '') == 'ignore_comment_on_edit'): ?>
+    <form method="post" class="" action="<?= $this->url->href('CostController', 'saveWithoutComment', array('plugin' => 'CostControl')) ?>" autocomplete="on">
+        <?= $this->form->csrf() ?>
+        <?= $this->form->label(t('Currency'), 'currency') ?>
+        <?= $this->form->select('currency', $currencies, $values, $errors) ?>
+        <?= $this->form->label(t('Rate'), 'rate') ?>
+        <?= $this->form->text('rate', $values, $errors, array('autofocus'), 'form-numeric') ?>
+        <?= $this->modal->submitButtons() ?>
+    </form>
+<?php endif ?>

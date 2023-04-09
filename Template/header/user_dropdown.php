e2e39fde25ff86ffa47e76267eb0b8d5160f475d
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-09 03:17:47 +0100

    Fix CSS Hack

--- Template/header/user_dropdown.php
+++ Template/header/user_dropdown.php
@@ -1,3 +1,3 @@
-<li class="">
+<li class="cost-control-dropdown">
     <?= $this->url->icon('question-circle', t('Cost Control'), 'CostController', 'showEveryone', ['plugin' => 'CostControl', ]) ?>
 </li>

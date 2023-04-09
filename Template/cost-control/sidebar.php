588b167515405555ed188de8fb4a39dd6d2d820d
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-09 05:14:17 +0100

    Show Link to Settings in Cost Control Sidebar

--- Template/cost-control/sidebar.php
+++ Template/cost-control/sidebar.php
@@ -3,6 +3,9 @@
         <li <?= $this->app->checkMenuSelection('CostController') ?>>
             <?= $this->url->link(t('Currency Rates'), 'CostController', 'showEveryone', array('plugin' => 'CostControl'), false, 'cost-control-menu-item') ?>
         </li>
+        <li class="">
+            <?= $this->url->link(t('Settings'), 'ConfigController', 'application', array(), false, '', '', false, 'CostControlSettings') ?>
+        </li>
         <?= $this->hook->render('template:cost-control:sidebar') ?>
     </ul>
 </div>

940191f2d3262ece22495907a4d0649531686d1e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-10 21:01:55 +0100

    Rough draft of edit_comment.php

--- Template/cost-control/edit_comment.php
+++ Template/cost-control/edit_comment.php
@@ -3,4 +3,11 @@
 </div>
 <form method="post" class="" action="<?= $this->url->href('CostController', 'saveCommentForm', array('plugin' => 'CostControl')) ?>" autocomplete="on">
     <?= $this->form->csrf() ?>
+
+    <?= $this->form->hidden($currency, array('currency')) ?>
+    edit comment for: <?= $currency ?>
+
+    <?= $this->form->label(t('New Comment'), 'comment') ?>
+    <?= $this->form->text('comment', $values, array(), array('placeholder="'. $comment .'"', 'value="'. $comment .'"'), 'form-text') ?>
+    <?= $this->modal->submitButtons() ?>
 </form>

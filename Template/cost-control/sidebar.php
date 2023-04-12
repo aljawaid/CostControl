724b6760b26aa715cc5d2082fa1d9e1b547e00a0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-12 05:23:36 +0100

    Show Settings menu only if allowed access

--- Template/cost-control/sidebar.php
+++ Template/cost-control/sidebar.php
@@ -3,9 +3,11 @@
         <li <?= $this->app->checkMenuSelection('CostController') ?>>
             <?= $this->url->link(t('Currency Rates'), 'CostController', 'showEveryone', array('plugin' => 'CostControl'), false, 'cost-control-menu-item') ?>
         </li>
-        <li class="">
-            <?= $this->url->link(t('Settings'), 'ConfigController', 'application', array(), false, '', '', false, 'CostControlSettings') ?>
-        </li>
+        <?php if ($this->user->hasAccess('ConfigController', 'application')): ?>
+            <li class="">
+                <?= $this->url->link(t('Settings'), 'ConfigController', 'application', array(), false, '', '', false, 'CostControlSettings') ?>
+            </li>
+        <?php endif ?>
         <?= $this->hook->render('template:cost-control:sidebar') ?>
     </ul>
 </div>

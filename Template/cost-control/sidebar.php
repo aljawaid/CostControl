6d62ceaf99e646fc57893be612bca595c586fbf7
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-14 03:49:36 +0100

    Add Hourly Rate to Sidebar

--- Template/cost-control/sidebar.php
+++ Template/cost-control/sidebar.php
@@ -3,6 +3,11 @@
         <li <?= $this->app->checkMenuSelection('CostController') ?>>
             <?= $this->url->link(t('Currency Rates'), 'CostController', 'showEveryone', array('plugin' => 'CostControl'), false, 'cost-control-menu-item') ?>
         </li>
+        <?php if ($this->user->hasAccess('HourlyRateController', 'index')): ?>
+            <li <?= $this->app->getRouterController() === 'HourlyRateController' ? 'class="active"' : '' ?>>
+                <?= $this->url->link(t('Hourly rate'), 'HourlyRateController', 'show', array('plugin' => 'CostControl', 'user_id' => $user['id'])) ?>
+            </li>
+        <?php endif ?>
         <?php if ($this->user->hasAccess('ConfigController', 'application')): ?>
             <li class="">
                 <?= $this->url->link(t('Settings'), 'ConfigController', 'application', array(), false, '', '', false, 'CostControlSettings') ?>

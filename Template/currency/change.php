3264ae3cac288626d09751fcf61b5122aed0b497
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-14 03:44:23 +0100

    Update Modal change.php

--- Template/currency/change.php
+++ Template/currency/change.php
@@ -1,10 +1,12 @@
-<div class="page-header">
-    <h2 class=""><?= t('Change Base Currency') ?></h2>
+<div id="ChangeModal" class="cost-control-page-header-change">
+    <h2 class="">
+        <span class="base-rate-home-icon"></span> <?= t('Change Base Currency') ?>
+    </h2>
+    <form method="post" class="change-base-rate" action="<?= $this->url->href('CostController', 'update', array('plugin' => 'CostControl')) ?>" autocomplete="on">
+        <?= $this->form->csrf() ?>
+        <?= $this->form->label(t('Base Currency'), 'application_currency') ?>
+        <?= $this->form->select('application_currency', $currencies, $values, $errors) ?>
+        <p class="form-help"><?= t('The application currency is known as the base currency and associates the base rate (1.00) against other currencies') ?></p>
+        <?= $this->modal->submitButtons() ?>
+    </form>
 </div>
-<form method="post" class="" action="<?= $this->url->href('CostController', 'update', array('plugin' => 'CostControl')) ?>" autocomplete="on">
-
-    <?= $this->form->csrf() ?>
-    <?= $this->form->label(t('Base Currency'), 'application_currency') ?>
-    <?= $this->form->select('application_currency', $currencies, $values, $errors) ?>
-    <?= $this->modal->submitButtons() ?>
-</form>

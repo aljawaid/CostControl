ba03ffa816ef71f92fd255a41f4f2968bc4ee92f
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-15 01:41:28 +0100

    NEW: Add Cost Control Sidebar Menu Item to Budget

--- Template/budget/sidebar.php
+++ Template/budget/sidebar.php
@@ -1,13 +1,16 @@
-<div class="sidebar">
-    <ul>
+<div class="sidebar budget-sidebar">
+    <ul class="">
         <li <?= $this->app->checkMenuSelection('BudgetController', 'show') ?>>
-            <?= $this->url->link(t('Budget overview'), 'BudgetController', 'show', array('plugin' => 'CostControl', 'project_id' => $project['id'])) ?>
+            <?= $this->url->link(t('Budget Overview'), 'BudgetController', 'show', array('plugin' => 'CostControl', 'project_id' => $project['id']), false, 'cost-control-menu-item') ?>
         </li>
         <li <?= $this->app->checkMenuSelection('BudgetLineController', 'show') ?>>
-            <?= $this->url->link(t('Budget lines'), 'BudgetLineController', 'show', array('plugin' => 'CostControl', 'project_id' => $project['id'])) ?>
+            <?= $this->url->link(t('Budget Lines'), 'BudgetLineController', 'show', array('plugin' => 'CostControl', 'project_id' => $project['id']), false, 'cost-control-menu-item') ?>
         </li>
         <li <?= $this->app->checkMenuSelection('BudgetController', 'breakdown') ?>>
-            <?= $this->url->link(t('Cost breakdown'), 'BudgetController', 'breakdown', array('plugin' => 'CostControl', 'project_id' => $project['id'])) ?>
+            <?= $this->url->link(t('Cost Breakdown'), 'BudgetController', 'breakdown', array('plugin' => 'CostControl', 'project_id' => $project['id']), false, 'cost-control-menu-item') ?>
+        </li>
+        <li <?= $this->app->checkMenuSelection('CostController') ?>>
+            <?= $this->url->link(t('Cost Control'), 'CostController', 'showEveryone', array('plugin' => 'CostControl'), false, 'cost-control-menu-item') ?>
         </li>
     </ul>
 </div>

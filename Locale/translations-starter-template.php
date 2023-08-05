fb648d10b25eadcaa39417870d417170c0fb3f32
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-05 22:58:45 +0100

    Fix Duplicate String `BudgetLineController.php`
    #7

--- Locale/translations-starter-template.php
+++ Locale/translations-starter-template.php
@@ -26,7 +26,6 @@ return array(
     // Controller/BudgetLineController.php
     //
     'Budget lines' => '',
-    'Budget lines' => '',
     'The budget line have been created successfully.' => '',
     'Unable to create the budget line.' => '',
     'Budget line removed successfully.' => '',

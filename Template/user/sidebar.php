4d1ab29300ff9a0c80b9aed5e507bdaaca13942b
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-21 22:07:45 +0100

    FIX: Translation String `sidebar.php`
    #7

--- Template/user/sidebar.php
+++ Template/user/sidebar.php
@@ -1,5 +1,5 @@
 <?php if ($this->user->hasAccess('HourlyRateController', 'index')): ?>
     <li <?= $this->app->getRouterController() === 'HourlyRateController' ? 'class="active"' : '' ?>>
-        <?= $this->url->link(t('Hourly rate'), 'HourlyRateController', 'show', array('plugin' => 'CostControl', 'user_id' => $user['id'])) ?>
+        <?= $this->url->link(t('Hourly Rate'), 'HourlyRateController', 'show', array('plugin' => 'CostControl', 'user_id' => $user['id'])) ?>
     </li>
 <?php endif ?>

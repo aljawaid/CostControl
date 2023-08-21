08376a8e1c6d590a4d7de7ca7bd7a31e9b11b380
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-21 09:41:49 +0100

    FIX: Translation Strings `edit_comment.php`
    #7

--- Template/cost-control/edit_comment.php
+++ Template/cost-control/edit_comment.php
@@ -3,7 +3,7 @@
         <h2 class="">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sticky-fill" viewBox="0 0 16 16">
                 <path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1h-11zm6 8.5a1 1 0 0 1 1-1h4.396a.25.25 0 0 1 .177.427l-5.146 5.146a.25.25 0 0 1-.427-.177V9.5z"/>
-            </svg> <?= t('Edit Comment for ') ?> <strong><?= $currency ?></strong>
+            </svg> <?= e('Edit Comment for %s', '<strong>' . $currency . '</strong>') ?>
         </h2>
         <form method="post" class="edit-create-comment" action="<?= $this->url->href('CostController', 'saveCommentForm', array('plugin' => 'costControl')) ?>" autocomplete="on">
             <?= $this->form->csrf() ?>
@@ -17,7 +17,7 @@
         <h2 class="">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sticky" viewBox="0 0 16 16">
                 <path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1h-11zM2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V8H9.5A1.5 1.5 0 0 0 8 9.5V14H2.5a.5.5 0 0 1-.5-.5v-11zm7 11.293V9.5a.5.5 0 0 1 .5-.5h4.293L9 13.793z"/>
-            </svg> <?= t('Add Comment for') ?> <strong><?= $currency ?></strong>
+            </svg> <?= e('Add Comment for %s', '<strong>' . $currency . '</strong>') ?>
         </h2>
         <form method="post" class="edit-create-comment" action="<?= $this->url->href('CostController', 'saveCommentForm', array('plugin' => 'costControl')) ?>" autocomplete="on">
             <?= $this->form->csrf() ?>
